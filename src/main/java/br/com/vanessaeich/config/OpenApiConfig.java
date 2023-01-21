6b490338796c2c7deae793425306d9083bd8d608
Vanessa Eich <vanessa.e@lyncas.net>
2023-01-21 15:37:36 -0300

    feat: adicionada open API

---
+++ src/main/java/br/com/vanessaeich/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package br.com.vanessaeich.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Vanessa Eich on 12/01/2023
+ */
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenApi(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("RESTful API- Spring Boot 3 and Java 18")
+                        .version("v1"));
+    }
+}
